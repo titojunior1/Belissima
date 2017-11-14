5ff47d3676dcd849bc743d62ec808fc7fa47248b
Moacir Tito <moacir.junior_vector@abril.com.br>
2017-11-14 11:16:01 -0200

    Alterações para tipo de produto e visibilidade

--- includes/models/Wpr/Kpl/ProdutosHakken.php
+++ includes/models/Wpr/Kpl/ProdutosHakken.php
@@ -131,6 +131,7 @@ class Model_Wpr_Kpl_ProdutosHakken extends Model_Wpr_Kpl_KplWebService {
 								'price' => $dados_produtos ['ValorVenda'], 
 								'special_price' => $dados_produtos ['ValorCusto'], 
 								//'tax_class_id' => 1, 
+								'type' => $dados_produtos['Tipo'],
 								'meta_title' => $dados_produtos ['Nome'], 
 								'additional_attributes' => array(
 							        'single_data' => array(
@@ -159,6 +160,7 @@ class Model_Wpr_Kpl_ProdutosHakken extends Model_Wpr_Kpl_KplWebService {
 							'url_key' => $dados_produtos ['Nome'], 
 							'visibility' => $dados_produtos ['Visibilidade'], 
 							//'tax_class_id' => 1, 
+							'type' => $dados_produtos['Tipo'],
 							'meta_title' => $dados_produtos ['Nome'],
 							'additional_attributes' => array(
 									        'single_data' => array(
@@ -236,14 +238,15 @@ class Model_Wpr_Kpl_ProdutosHakken extends Model_Wpr_Kpl_KplWebService {
 			$array_produtos [0] ['Descricao'] = utf8_encode ( empty ( $request ['DadosProdutos'] ['Descricao'] ) ? $request ['DadosProdutos'] ['NomeProduto'] : str_replace ( '<BR>', '', $request ['DadosProdutos'] ['Descricao'] ) );
 			$array_produtos [0] ['CodigoProdutoPai'] = isset ( $request ['DadosProdutos'] ['CodigoProdutoPai'] ) ? $request ['DadosProdutos'] ['CodigoProdutoPai'] : '';
 			$array_produtos [0] ['Unidade'] = isset ( $request ['DadosProdutos'] ['Unidade'] ) ? $request ['DadosProdutos'] ['Unidade'] : '';
-			$array_produtos [0] ['Visibilidade'] = 4; // Exibir produto Pai
 			
 			// verifica se produto é pai ou filho
-// 			if (strstr ( $request ['DadosProdutos'] ['CodigoProduto'], '-' ) == true) {
-// 				$array_produtos [0] ['Visibilidade'] = 1; // Não exibir pois é produto Filho
-// 			} else {
-// 				$array_produtos [0] ['Visibilidade'] = 4; // Exibir produto Pai
-// 			}
+			if (strstr ( $request ['DadosProdutos'] ['CodigoProduto'], '-' ) == true) {
+				$array_produtos [0] ['Visibilidade'] = 1; // Não exibir pois é produto Filho
+				$array_produtos [0] ['Tipo'] = 'simple'; 
+			} else {
+				$array_produtos [0] ['Visibilidade'] = 4; // Exibir produto Pai
+				$array_produtos [0] ['Tipo'] = 'configurable';
+			}
 		} else {
 			
 			foreach ( $request ["DadosProdutos"] as $i => $d ) {				
@@ -266,14 +269,16 @@ class Model_Wpr_Kpl_ProdutosHakken extends Model_Wpr_Kpl_KplWebService {
 				$array_produtos [$i] ['Descricao'] = utf8_encode ( empty ( $d ['Descricao'] ) ? $d ['NomeProduto'] : str_replace ( '<BR>', '', $d ['Descricao'] ) );
 				$array_produtos [$i] ['CodigoProdutoPai'] = isset ( $d ['CodigoProdutoPai'] ) ? $d ['CodigoProdutoPai'] : '';
 				$array_produtos [$i] ['Unidade'] = isset ( $d ['Unidade'] ) ? $d ['Unidade'] : '';
-				$array_produtos [$i] ['Visibilidade'] = 4; // Exibir produto Pai
 				
 				// verifica se produto é pai ou filho
-// 				if (strstr ( $d ['CodigoProduto'], '-' ) == true) {
-// 					$array_produtos [$i] ['Visibilidade'] = 1; // Não exibir pois é produto Filho
-// 				} else {
-// 					$array_produtos [$i] ['Visibilidade'] = 4; // Exibir produto Pai
-// 				}
+				if (strstr ( $d ['CodigoProduto'], '-' ) == true) {
+					$array_produtos [$i] ['Visibilidade'] = 1; // Não exibir pois é produto Filho
+					$array_produtos [$i] ['Tipo'] = 'simple';
+					
+				} else {
+					$array_produtos [$i] ['Visibilidade'] = 4; // Exibir produto Pai
+					$array_produtos [$i] ['Tipo'] = 'configurable';
+				}
 			}
 		}
 		
